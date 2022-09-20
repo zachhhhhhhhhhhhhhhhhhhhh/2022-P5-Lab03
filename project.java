9790a43968f38810c2aa815b1065b46f7967ef7b
zach <zachary.schwerdtfeger@vtsd.com>
2022-09-20 12:35:45 -0400

    80 points

---
+++ project.java
@@ -0,0 +1,18 @@
+public class project {
+    public static void main(String args[]) {
+    
+    int m1 =20000;
+    int s1 =2000;
+    int s2 =20;
+
+    System.out.println("starting seconds " + m1);
+
+    System.out.print(m1/3600);  System.out.println(":hours");
+
+    System.out.print(s1/60); System.out.println(":minutes");
+    System.out.print(s2);  System.out.println(":seconds");
+
+
+    }
+
+}
