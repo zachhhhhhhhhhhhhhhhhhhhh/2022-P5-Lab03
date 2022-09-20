fed6926353e8bc93420e17f8d511180dee119159
zach <zachary.schwerdtfeger@vtsd.com>
2022-09-20 12:54:50 -0400

    80 points

--- project.java
+++ project.java
@@ -1,16 +1,22 @@
 public class project {
     public static void main(String args[]) {
     
-    int m1 =20000;
-    int s1 =2000;
-    int s2 =20;
+    int sv =20000;
 
-    System.out.println("starting seconds " + m1);
 
-    System.out.print(m1/3600);  System.out.println(":hours");
+    int hrs = sv / 3600;
+    int sec = sv % 3600;
+    int min = sec / 60;
+    int rsec = sec % 60;
 
-    System.out.print(s1/60); System.out.println(":minutes");
-    System.out.print(s2);  System.out.println(":seconds");
+    System.out.println("starting value: " + sv);
+    System.out.println("hours: " + hrs);
+    System.out.println("minutes: " + min);
+    System.out.println("seconds: " + rsec);
+
+
+
+
 
 
     }
