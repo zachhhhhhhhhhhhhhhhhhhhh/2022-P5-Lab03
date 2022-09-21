56180351e61f5454f8ca1ecc30697cd24553546b
zach <zachary.schwerdtfeger@vtsd.com>
2022-09-21 12:26:36 -0400

    80 points

--- project.java
+++ project.java
@@ -1,7 +1,7 @@
 public class project {
     public static void main(String args[]) {
     
-    int sv =20000;
+    int sv =10000;
 
 
     int hrs = sv / 3600;
@@ -13,10 +13,24 @@ public class project {
     System.out.println("hours: " + hrs);
     System.out.println("minutes: " + min);
     System.out.println("seconds: " + rsec);
+    System.out.println();
+    System.out.println();
+    //100 points
 
+        System.out.println("100 points");
+        int smil = 10000123;
+        int hours = smil / 3600000;
+        int milliseconds = smil % 3600000;
+        int minutes = milliseconds / 60000;
+        int remainmill = milliseconds % 60000;
+        int seconds = remainmill / 1000;
+        int remmilliseconds = remainmill % 1000;
 
-
-
+        System.out.println("starting value: " + smil);
+        System.out.println("hours: " + hours);
+        System.out.println("minutes: " + minutes);
+        System.out.println("seconds: " + seconds);
+        System.out.println("milliseconds: " + remmilliseconds);
 
 
     }
